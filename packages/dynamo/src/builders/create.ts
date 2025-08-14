815962ddad4f2c38d209c2113825d9f78acccae8
Igor Souza <igorsouza.96@hotmail.com>
2025-08-14 09:52:47 -0300

    chore: update CI workflow to include Biome linting and formatting checks

--- packages/dynamo/src/builders/create.ts
+++ packages/dynamo/src/builders/create.ts
@@ -1,60 +1,52 @@
-import { PutCommand, type PutCommandInput } from '@aws-sdk/lib-dynamodb';
-import type { z } from 'zod';
-import { EntityValidationError, MissingKeyError } from '../errors.js';
-import type { ConnectedTable } from '../table/connection.js';
-import type { EntitySchemaDefinition } from '../types.js';
-import { serialize } from '../utils/transformer.js';
-import { BaseBuilder, type DynamoResult } from './base-builder.js';
+import { PutCommand, type PutCommandInput } from "@aws-sdk/lib-dynamodb";
+import { ZodError, type z } from "zod";
+import { EntityValidationError, MissingKeyError } from "../errors.js";
+import type { ConnectedTable } from "../table/connection.js";
+import type { EntitySchemaDefinition } from "../types.js";
+import { serialize } from "../utils/transformer.js";
+import { BaseBuilder, type DynamoResult } from "./base-builder.js";
 
-export class EntityCreateBuilder<
-  TSchema extends z.ZodObject<EntitySchemaDefinition>,
-> extends BaseBuilder<z.infer<TSchema>> {
-  private _item: z.infer<TSchema> | undefined;
-  private _condition?: string;
-  private schema: TSchema;
+export class EntityCreateBuilder<TSchema extends z.ZodObject<EntitySchemaDefinition>> extends BaseBuilder<
+	z.infer<TSchema>
+> {
+	private _item: z.infer<TSchema> | undefined;
+	private _condition?: string;
+	private schema: TSchema;
 
-  constructor(table: ConnectedTable, schema: TSchema) {
-    super(table);
-    this.schema = schema;
-  }
+	constructor(table: ConnectedTable, schema: TSchema) {
+		super(table);
+		this.schema = schema;
+	}
 
-  item(data: z.input<TSchema>) {
-    this._item = this.schema.parse(data);
-    return this;
-  }
+	item(data: z.input<TSchema>) {
+		this._item = this.schema.parse(data);
+		return this;
+	}
 
-  condition(expression: string) {
-    this._condition = expression;
-    return this;
-  }
+	condition(expression: string) {
+		this._condition = expression;
+		return this;
+	}
 
-  async exec(): Promise<DynamoResult<z.TypeOf<TSchema>>> {
-    if (!this._item) {
-      return [
-        null,
-        new MissingKeyError('No item provided for create operation.'),
-      ];
-    }
-    const serializedItem = serialize(this._item, this.table.options.caseStyle);
-    const params: PutCommandInput = {
-      TableName: this.table.tableName,
-      Item: serializedItem,
-    };
-    if (this._condition) {
-      params.ConditionExpression = this._condition;
-    }
-    try {
-      const [_, opErr] = await this.send(new PutCommand(params));
-      if (opErr) return [null, opErr];
-      return [this._item, null];
-    } catch (err) {
-      return [
-        null,
-        new EntityValidationError(
-          'Entity validation failed',
-          (err as any)?.issues
-        ),
-      ];
-    }
-  }
+	async exec(): Promise<DynamoResult<z.TypeOf<TSchema>>> {
+		if (!this._item) {
+			return [null, new MissingKeyError("No item provided for create operation.")];
+		}
+		const serializedItem = serialize(this._item, this.table.options.caseStyle);
+		const params: PutCommandInput = {
+			TableName: this.table.tableName,
+			Item: serializedItem,
+		};
+		if (this._condition) {
+			params.ConditionExpression = this._condition;
+		}
+		try {
+			const [_, opErr] = await this.send(new PutCommand(params));
+			if (opErr) return [null, opErr];
+			return [this._item, null];
+		} catch (err) {
+			const issues = err instanceof ZodError ? (err as ZodError).issues : undefined;
+			return [null, new EntityValidationError("Entity validation failed", issues)];
+		}
+	}
 }
